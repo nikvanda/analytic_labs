cf68b4bd049d9b3e9de22b1bbe76ee60a217f480
Nikita Vanda <nikitavanda149@proton.me>
2024-09-17 13:55:22 +0300

    add 1-st task solution

---  lab_1/lab_1.py
+++  lab_1/lab_1.py
@@ -1,11 +1,16 @@
+from sklearn import datasets, preprocessing
 
-def ex1():
-    pass
+
+def ex1(dataset):
+    processed_data = dataset.iloc[10:15]
+    print(processed_data)
+    print(processed_data.describe())
+    dataset.to_csv('ex1.csv')
 
 
 def main():
-
-    ex1()
+    iris = datasets.load_iris(as_frame=True).frame
+    ex1(iris)
 
 
 if __name__ == '__main__':
