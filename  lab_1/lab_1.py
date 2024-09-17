44791b0ef0e41410db4307cdb10c7dc107249d81
Nikita Vanda <nikitavanda149@proton.me>
2024-09-17 14:09:53 +0300

    add ex2 solution

---  lab_1/lab_1.py
+++  lab_1/lab_1.py
@@ -1,8 +1,14 @@
 from sklearn import datasets, preprocessing
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 class Exercise:
-    iris = datasets.load_iris(as_frame=True).frame
+    __iris = datasets.load_iris(as_frame=True).frame
+
+    @property
+    def iris(self):
+        return self.__iris
 
     def ex1(self):
         processed_data = self.iris.iloc[10:15]
@@ -11,12 +17,19 @@ class Exercise:
         self.iris.to_csv('ex1.csv')
 
     def ex2(self):
-        pass
+        sns.scatterplot(data=self.iris, x='petal length (cm)', y='petal width (cm)', hue='target', palette='Greys')
+        sns.pairplot(self.iris, hue='target')
+        plt.show()
+
+        confusion_matrix = self.iris.drop('target', axis=1).corr()
+        sns.heatmap(confusion_matrix)
+        plt.show()
 
 
 def main():
     ex = Exercise()
-    ex.ex1()
+    # ex.ex1()
+    ex.ex2()
 
 
 if __name__ == '__main__':
