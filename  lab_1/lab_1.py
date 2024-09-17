335984b91b5d49a4bc791db8f9f4447443292f16
Nikita Vanda <nikitavanda149@proton.me>
2024-09-17 13:59:42 +0300

    rewrite to class-based

---  lab_1/lab_1.py
+++  lab_1/lab_1.py
@@ -1,16 +1,22 @@
 from sklearn import datasets, preprocessing
 
 
-def ex1(dataset):
-    processed_data = dataset.iloc[10:15]
-    print(processed_data)
-    print(processed_data.describe())
-    dataset.to_csv('ex1.csv')
+class Exercise:
+    iris = datasets.load_iris(as_frame=True).frame
+
+    def ex1(self):
+        processed_data = self.iris.iloc[10:15]
+        print(processed_data)
+        print(processed_data.describe())
+        self.iris.to_csv('ex1.csv')
+
+    def ex2(self):
+        pass
 
 
 def main():
-    iris = datasets.load_iris(as_frame=True).frame
-    ex1(iris)
+    ex = Exercise()
+    ex.ex1()
 
 
 if __name__ == '__main__':
